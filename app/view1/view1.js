9a2360c803b87bb6f9e3bf4e7ebfb458b24b49df
mikeysol <mikey.s.barnes@gmail.com>
2015-10-20 20:10:29 -0700

    adding nested templates to enhance code reuse and readability

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,8 +1,8 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+var resume = angular.module('myApp.view1', ['ngRoute']);
 
-.config(['$routeProvider', function($routeProvider) {
+resume.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
     controller: 'View1Ctrl'
