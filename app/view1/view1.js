f3215339ae908c5b3f5d16b3aceee1e4da60e45f
mikeysol <mikey.s.barnes@gmail.com>
2015-10-28 16:31:13 -0700

    updated .gitignore, css file, and templates

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -13,4 +13,14 @@ resume.config(['$routeProvider', function($routeProvider) {
       $http.get('resume/michael-barnes-resume.json').success(function(data){
         $scope.resumes = data;
       });
+        $scope.dragControlListeners = {
+            //accept: function (sourceItemHandleScope, destSortableScope) {return true;},//override to determine drag is allowed or not. default is true.
+            itemMoved: function (event) {
+                event.source.itemScope.modelValue.status = event.dest.sortableScope.$parent.column.name;
+            },
+            orderChanged: function(event) {
+                },
+            containment: '#board',//optional param.
+            clone: true //optional param for clone feature.
+        };
 }]);
