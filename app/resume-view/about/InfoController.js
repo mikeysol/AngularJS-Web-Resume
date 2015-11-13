78fe9195ede534a91fd18839177eaf658049efae
mikeysol <mikey.s.barnes@gmail.com>
2015-11-12 23:38:08 -0800

    changed intro

--- app/resume-view/about/InfoController.js
+++ app/resume-view/about/InfoController.js
@@ -6,13 +6,13 @@
 var nav = angular.module('myApp.resume');
 
 nav.controller('InfoCtrl', ['$scope','$interval', function($scope,$interval){
-    $scope.data = ["Hi, I am <b>Michael Barnes</b> and I developed this small WebApp in <b>AngularJS</b> featuring my resume" +
-    " to help recruiters or hiring managers get to know me. <br/> <br/>" +
+    $scope.data = ["Hi, I am <b>Michael Barnes</b> and this is a small demo in <b>AngularJS</b> that turns a standard resume" +
+    " into something more fun. <br/> <br/>" +
     "<ul><li>You can <b>filter</b> out sections via the search bar</li>" +
     "<li><b>Reorder</b> the sections via the Section View panel</li>" +
     "<li>At the top of page <b>navigate to different perspectives</b> such as Web or all Software Engineering.</li>" +
     "<li><b>Toggle colored Buzz Words</b> on/off to help scan for desired skills</li></ul>" +
-    "I will be updating this app and adding features over time. Check out the source code on my gitHub.<br/>" +
+    "I will be adding features over time. Check out the source code on my gitHub.<br/>" +
     "If you're <b>hiring</b> then connect with me on LinkedIn or send me an email.<br/>" +
     "Thanks and enjoy!"];
     $scope.index = 0;
